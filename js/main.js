23d60dd1c5569834ec4273efc86dd29aa26a577c
kosuke928 <goodrelationship928@gmail.com>
2024-06-29 08:20:25 +0900

    Works完成

--- js/main.js
+++ js/main.js
@@ -13,5 +13,48 @@ jQuery(function ($) {
     $(".p-dnav__item").on("click", function () {
       $("#js-drawer-open").removeClass("is-open");
     });
+
+    const parentGallery = document.querySelector(".p-slide__wrapper");
+    const cloneGallery = parentGallery.cloneNode(true);
+    const cloneChildren = cloneGallery.querySelectorAll(".p-slide__slide");
+    cloneChildren.forEach((cloneChild) => {
+      parentGallery.appendChild(cloneChild.cloneNode(true));
+    });
+
+    //Spots Swiper
+    var swiper = new Swiper(".works-swiper", {
+      loop: true,
+      initialSlide: 1,
+      slidesPerView: 1.58,
+      spaceBetween: 20,
+      speed: 500,
+      centeredSlides: true,
+      scrollbar: {
+        el: '.p-slide__bar',
+        hide: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        512: {
+          spaceBetween: 25,
+          slidesPerView: 2,
+        },
+        768: {
+          spaceBetween: 30,
+          slidesPerView: 2.4,
+        },
+        1000: {
+          spaceBetween: 45,
+          slidesPerView: 2.6,
+        },
+        1280: {
+          spaceBetween: 45,
+          slidesPerView: 2.85,
+        },
+      },
+    });
   });
 });
